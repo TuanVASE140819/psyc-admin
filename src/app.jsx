48ad78d86834574bb0b9ce1ba1b5ff3c316c6c4c
Phuoc Tai <phuoctai12t@gmai.com>
2022-12-18 20:56:46 +0700

    default theme

--- src/app.jsx
+++ src/app.jsx
@@ -81,52 +81,54 @@ export const layout = ({ initialState, setInitialState }) => {
       return (
         <>
           {children}
-          {/* {!props.location?.pathname?.includes('/login') && (
-            <SettingDrawer
-              // tiếng việt
-              locale={{
-                'vi-VN': {
-                  'app.setting.pagestyle': 'Cài đặt kiểu tổng thể',
-                  'app.setting.pagestyle.dark': 'phong cách menu tối',
-                  'app.setting.pagestyle.light': 'Phong cách menu tươi sáng',
-                  'app.setting.content-width': 'Độ rộng nội dung',
-                  'app.setting.content-width.fixed': 'Cố định',
-                  'app.setting.content-width.fluid': 'Lỏng lẻo',
-                  'app.setting.themecolor': 'Cài đặt màu chủ đề',
-                  'app.setting.themecolor.dust': 'Màu bụi',
-                  'app.setting.themecolor.volcano': 'Màu núi lửa',
-                  'app.setting.themecolor.sunset': 'Màu hoàng hôn',
-                  'app.setting.themecolor.cyan': 'Màu lam',
-                  'app.setting.themecolor.green': 'Màu xanh lá cây',
-                  'app.setting.themecolor.daybreak': 'Màu bình minh (mặc định)',
-                  'app.setting.themecolor.geekblue': 'Màu xanh da trời',
-                  'app.setting.themecolor.purple': 'Màu tím',
-                  'app.setting.navigationmode': 'Chế độ điều hướng',
-                  'app.setting.sidemenu': 'Bố cục menu bên trái',
-                  'app.setting.topmenu': 'Bố cục menu trên cùng',
-                  'app.setting.fixedheader': 'Đầu trang cố định',
-                  'app.setting.fixedsidebar': 'Thanh bên cố định',
-                  'app.setting.fixedsidebar.hint': 'Chỉ có bố cục menu bên trái mới có hiệu lực',
-                  'app.setting.hideheader': 'Ẩn đầu trang khi cuộn',
-                  'app.setting.hideheader.hint': 'Chỉ có khi đầu trang cố định mới có hiệu lực',
-                  'app.setting.othersettings': 'Cài đặt khác',
-                  'app.setting.weakmode': 'Chế độ yếu',
-                  'app.setting.copy': 'Sao chép cấu hình',
-                  'app.setting.copyinfo':
-                    'Sao chép thành công，vui lòng thay thế src/defaultSettings.js',
-                  'app.setting.production.hint':
-                    'Cài đặt trang này chỉ có hiệu lực trong môi trường phát triển',
-                },
-              }}
-              onClear
-              disableUrlParams
-              enableDarkTheme
-              settings={initialState?.settings}
-              onSettingChange={(settings) => {
-                setInitialState((preInitialState) => ({ ...preInitialState, settings }));
-              }}
-            />
-          )} */}
+          <div style={{ visibility: 'hidden' }}>
+            {!props.location?.pathname?.includes('/login') && (
+              <SettingDrawer
+                // tiếng việt
+                locale={{
+                  'vi-VN': {
+                    'app.setting.pagestyle': 'Cài đặt kiểu tổng thể',
+                    'app.setting.pagestyle.dark': 'phong cách menu tối',
+                    'app.setting.pagestyle.light': 'Phong cách menu tươi sáng',
+                    'app.setting.content-width': 'Độ rộng nội dung',
+                    'app.setting.content-width.fixed': 'Cố định',
+                    'app.setting.content-width.fluid': 'Lỏng lẻo',
+                    'app.setting.themecolor': 'Cài đặt màu chủ đề',
+                    'app.setting.themecolor.dust': 'Màu bụi',
+                    'app.setting.themecolor.volcano': 'Màu núi lửa',
+                    'app.setting.themecolor.sunset': 'Màu hoàng hôn',
+                    'app.setting.themecolor.cyan': 'Màu lam',
+                    'app.setting.themecolor.green': 'Màu xanh lá cây',
+                    'app.setting.themecolor.daybreak': 'Màu bình minh (mặc định)',
+                    'app.setting.themecolor.geekblue': 'Màu xanh da trời',
+                    'app.setting.themecolor.purple': 'Màu tím',
+                    'app.setting.navigationmode': 'Chế độ điều hướng',
+                    'app.setting.sidemenu': 'Bố cục menu bên trái',
+                    'app.setting.topmenu': 'Bố cục menu trên cùng',
+                    'app.setting.fixedheader': 'Đầu trang cố định',
+                    'app.setting.fixedsidebar': 'Thanh bên cố định',
+                    'app.setting.fixedsidebar.hint': 'Chỉ có bố cục menu bên trái mới có hiệu lực',
+                    'app.setting.hideheader': 'Ẩn đầu trang khi cuộn',
+                    'app.setting.hideheader.hint': 'Chỉ có khi đầu trang cố định mới có hiệu lực',
+                    'app.setting.othersettings': 'Cài đặt khác',
+                    'app.setting.weakmode': 'Chế độ yếu',
+                    'app.setting.copy': 'Sao chép cấu hình',
+                    'app.setting.copyinfo':
+                      'Sao chép thành công，vui lòng thay thế src/defaultSettings.js',
+                    'app.setting.production.hint':
+                      'Cài đặt trang này chỉ có hiệu lực trong môi trường phát triển',
+                  },
+                }}
+                onClear
+                disableUrlParams
+                enableDarkTheme
+                settings={initialState?.settings}
+                onSettingChange={(settings) => {
+                  setInitialState((preInitialState) => ({ ...preInitialState, settings }));
+                }}
+              />
+            )}
+          </div>
         </>
       );
     },
